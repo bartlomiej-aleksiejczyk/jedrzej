9c51fec9bddc80f84bf144fae01c8436f0089e1e
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-04 22:05:16 +0200

    Remove redundant comments from code

--- scriptedpipeline/pipeline.py
+++ scriptedpipeline/pipeline.py
@@ -1,5 +1,3 @@
-# myapp/pipeline.py
-
 class PipelineStep:
     def execute(self, data):
         raise NotImplementedError("Each step must define an execute method.")
@@ -9,7 +7,7 @@ class PrintStep(PipelineStep):
         self.message = message
 
     def execute(self, data):
-        print(self.message)  # Or accumulate messages in a list to show in the web
+        print(self.message)  
         return data + self.message + "\n"
 
 class ModifyStep(PipelineStep):
@@ -19,7 +17,6 @@ class ModifyStep(PipelineStep):
     def execute(self, data):
         return data + self.append_text + "\n"
 
-# Map of allowed steps and their corresponding classes
 step_classes = {
     'PrintStep': PrintStep,
     'ModifyStep': ModifyStep,
@@ -27,7 +24,7 @@ step_classes = {
 
 def process_script(script):
     steps = parse_script(script)
-    data = ""  # Initial data passed through the pipeline
+    data = "" 
     for step in steps:
         step_type, args = step.split(':', 1)
         step_class = step_classes.get(step_type.strip())
@@ -37,5 +34,4 @@ def process_script(script):
     return data
 
 def parse_script(script):
-    # Each step is on a new line
     return script.strip().split('\n')
